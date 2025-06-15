6b61115b8fbdabd7e8ed4c2613eee2d9a14e63cc
xsiles13 <01silesxavier@gmail.com>
2025-06-15 18:00:10 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,18 @@
 from app import crear_aplicacion, basedatos
 from pathlib import Path
+import os
 
+# Crear la aplicación
 aplicacion = crear_aplicacion()
 
-# Crear la base de datos en /instance la primera vez
+# Crear la carpeta 'instance' si no existe
+Path("instance").mkdir(exist_ok=True)
+
+# Crear la base de datos si no existe
 with aplicacion.app_context():
     if not Path("instance/torneo.db").exists():
         basedatos.create_all()
-        print("✅ Base de datos creada correctamente.")
+        print("Base de datos creada correctamente.")
 
 if __name__ == "__main__":
     aplicacion.run(debug=True)
